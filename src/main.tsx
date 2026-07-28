473d06aca2e3ff7f4b16d8175f2b2b96742d4a8b
Shangomania <shangomania69@gmail.com>
2026-07-28 18:50:09 +0400

    fix: configure router base path

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/orchidia-studio">
       <App />
     </BrowserRouter>
   </StrictMode>,
